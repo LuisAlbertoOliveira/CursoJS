8d71e7de36cfe9c66bd2c23ebc7f29808e4e4560
Luis Alberto Pires de Oliveira <oliv.luis@hotmail.com>
2020-04-29 08:52:16 -0300

    basico de  condicional 1 JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidde do seu carro é ${vel}Km/h`)
+if (vel > 60){ //condição simples. somente uma analise; sem o senao.
+    console.log('Voce ultrapassou a velocidade máxima! MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
